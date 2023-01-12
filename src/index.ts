7fa434467b96610a080aebfb4307be0e0496600e
Oleg Brezgalov <brezgalov.developer@gmail.com>
2023-01-12 18:52:49 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import test2 from "./test2";
+
+function test1() {
+    console.log('test1');
+};
+
+test1();
+test2();
---
+++ src/test2.ts
@@ -0,0 +1,4 @@
+export default function test2()
+{
+    console.log('test2');
+}
